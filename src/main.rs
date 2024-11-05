049f7dbd67460ffd11c2c1f717973a0814d7d8b6
PyMati <mateuszcodedeveloper@gmail.com>
2024-11-05 08:29:44 +0100

    Adding automatic running to QEMU, println macro and creating Writer impl for VGA Text buffer. Starting writing framework for tests

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,73 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+mod serial;
+mod vga_buffer;
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
 
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga = 0xb8000 as *mut u8;
+    println!("SIEMANKO");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga.offset(i as isize * 2) = byte;
-            *vga.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
